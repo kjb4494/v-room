22d275b5d22b6e13ecc1bffcde55b20d40d059de
khsFirefox <rlagnltn1357@gmail.com>
2025-07-29 21:22:12 +0900

    eslint 설정

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { RoomService } from './room.service';
 import { RoomController } from './room.controller';
+import { RoomService } from './room.service';
 
 @Module({
   providers: [RoomService],
-  controllers: [RoomController]
+  controllers: [RoomController],
 })
 export class RoomModule {}
